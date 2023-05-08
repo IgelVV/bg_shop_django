3e7cd73b643f67b2bcab88d5608c11e3501cbfb5
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-05-08 21:24:13 +0400

    Profile model

--- bg_shop/account/models.py
+++ bg_shop/account/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from account import validators
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Meta:
+        verbose_name = _("profile")
+        verbose_name_plural = _("profiles")
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("user"),
+    )
+    phone_number = models.CharField(
+        validators=[
+            validators.PhoneRegexValidator(
+                message=_("Phone number must be 10 digits."))
+        ],
+        max_length=10,
+        blank=True,
+        verbose_name=_("phone number"),
+    )
+    # avatar = models.ForeignKey(  # todo ImageField (или оставить один класс для всех фоток)
+    #     "shop.Image",
+    #     on_delete=models.SET_NULL,
+    #     null=True,
+    #     blank=True,
+    #     verbose_name=_("avatar"),
+    # )
+
