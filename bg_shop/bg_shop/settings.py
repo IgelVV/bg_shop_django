bb606917944b015b9ce9cc6103a63d5203eb06e4
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-05-11 19:07:47 +0400

    drf permission is changed

--- bg_shop/bg_shop/settings.py
+++ bg_shop/bg_shop/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ]
     # "EXCEPTION_HANDLER": "styleguide_example.api.exception_handlers.drf_default_with_modifications_exception_handler",
     # # 'EXCEPTION_HANDLER': 'styleguide_example.api.exception_handlers.hacksoft_proposed_exception_handler',
