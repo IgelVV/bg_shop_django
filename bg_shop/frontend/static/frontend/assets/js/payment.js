6b6f502ea1a5e8ae85b487c5daa975599b44888b
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-06-21 01:58:51 +0400

    payment.js getting orderId is changed

--- bg_shop/frontend/static/frontend/assets/js/payment.js
+++ bg_shop/frontend/static/frontend/assets/js/payment.js
@@ -2,9 +2,26 @@ var mix = {
 	methods: {
 		submitPayment() {
 			console.log('submitPayment is called')
-			const orderId = location.pathname.startsWith('/payment/')
-				? Number(location.pathname.replace('/payment/', '').replace('/', ''))
-				: null
+//			const orderId = location.pathname.startsWith('/payment/')
+//				? Number(location.pathname.replace('/payment/', '').replace('/', ''))
+//				: null
+			if (location.pathname.startsWith('/payment/')) {
+				var orderId = Number(
+					location.pathname
+						.replace('/payment/', '')
+						.replace('/', '')
+				)
+			}
+			else if (location.pathname.startsWith('/payment-someone/')) {
+				var orderId = Number(
+					location.pathname
+						.replace('/payment-someone/', '')
+						.replace('/', '')
+				)
+			}
+			else {
+				var orderId = null
+			}
 			this.postData(`/api/payment/${orderId}/`, {
 				name: this.name,
 				number: this.number,
@@ -13,7 +30,7 @@ var mix = {
 				code: this.code
 			})
 				.then(() => {
-					alert('Успешная оплата')
+					alert('Ждём подтверждения оплаты от платёжной системы')
 					this.number = ''
 					this.name = ''
 					this.year = ''
@@ -23,6 +40,7 @@ var mix = {
 				})
 				.catch(() => {
 					console.warn('Ошибка при оплате')
+					alert('Ошибка при оплате')
 				})
 		}
 	},
