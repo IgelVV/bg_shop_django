143be695da3ac9e766558a7cddb25104509dbf7c
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-06-22 21:59:38 +0400

    transaction atomic

--- bg_shop/payment/webhooks.py
+++ bg_shop/payment/webhooks.py
@@ -3,6 +3,8 @@ import json
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.db import transaction
+
 from orders.models import Order
 from payment import models, enums
 
@@ -18,25 +20,26 @@ def stripe_webhook(request):
     order_id = payload.get("order_id", None)
     errors = payload.get("errors", None)
     payment_id = payload.get("payment_id", None)
+    print(payment_id)
 
     order = Order.objects.get(pk=order_id)
     if order.paid:
         return HttpResponse(status=409)
-
-    match status:
-        case enums.PaymentStatuses.SUCCESS.value:
-            order.paid = True
-            order.full_clean()
-            order.save()
-            payment = models.Payment(
-                order_id=order_id,
-                payment_id=payment_id,)
-            payment.full_clean()
-            payment.save()
-        case enums.PaymentStatuses.FAIL.value:
-            order.status = Order.Statuses.REJECTED
-            order.full_clean()
-            order.save()
-        case _:
-            return HttpResponse(status=400)
+    with transaction.atomic():
+        match status:
+            case enums.PaymentStatuses.SUCCESS.value:
+                order.paid = True
+                order.full_clean()
+                order.save()
+                payment = models.Payment(
+                    order_id=order_id,
+                    payment_id=payment_id,)
+                payment.full_clean()
+                payment.save()
+            case enums.PaymentStatuses.FAIL.value:
+                order.status = Order.Statuses.REJECTED
+                order.full_clean()
+                order.save()
+            case _:
+                return HttpResponse(status=400)
     return HttpResponse(status=200)
