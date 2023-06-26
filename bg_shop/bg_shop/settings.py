59514b61c300ab31466cf2138961ad882300d539
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-06-26 18:52:19 +0400

    .env is implemented

--- bg_shop/bg_shop/settings.py
+++ bg_shop/bg_shop/settings.py
@@ -9,22 +9,36 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!2@7xmj*!-=2@(3mqw=t9&abk7zw)ojdzaywc&hmhki6rg!vm9'
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-!2@7xmj*!-=2@(3mqw=t9&abk7zw)ojdzaywc&hmhki6rg!vm9",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "0.0.0.0",
+] + getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "0.0.0.0",
+]
 
 # Application definition
 
