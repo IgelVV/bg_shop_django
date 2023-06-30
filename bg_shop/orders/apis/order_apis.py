36c2acfdd6eb7d84d86b248aca236db69ea74ba5
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-06-30 19:08:01 +0400

    comment allow blank

--- bg_shop/orders/apis/order_apis.py
+++ bg_shop/orders/apis/order_apis.py
@@ -113,7 +113,7 @@ class OrderDetailApi(views.APIView):
         status = drf_serializers.CharField()
         city = drf_serializers.CharField()
         address = drf_serializers.CharField()
-        comment = drf_serializers.CharField()
+        comment = drf_serializers.CharField(allow_null=True, allow_blank=True)
         products = serializers.OrderedProductInputSerializer(
             many=True, required=False)
 
