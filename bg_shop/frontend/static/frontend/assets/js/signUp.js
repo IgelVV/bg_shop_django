eef9fdfd4bcc49ed55a8c65e45ea52e3307ecd9c
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-07-04 18:00:45 +0400

    signUp.html reply password

--- bg_shop/frontend/static/frontend/assets/js/signUp.js
+++ bg_shop/frontend/static/frontend/assets/js/signUp.js
@@ -4,6 +4,17 @@ var mix = {
 			const name = document.querySelector('#name').value
 			const username = document.querySelector('#login').value
 			const password = document.querySelector('#password').value
+			const passwordReply = document.querySelector('#passwordReply').value
+			if (
+			    !name.trim().length ||
+			    !username.trim().length ||
+                !password.trim().length ||
+                !passwordReply.trim().length ||
+                password !== passwordReply
+            ) {
+                alert('В форме присутствуют незаполненные поля или пароли не совпадают')
+                return
+            }
 			// this.postData('/api/sign-up/', JSON.stringify({ name, username, password }))
 			this.postData('/api/sign-up/', {
 				name: name,
