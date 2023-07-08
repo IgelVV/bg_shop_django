3e4f1ccd49c5195ac230806a38b8f678dbe1f05a
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-07-08 20:50:21 +0400

    test_split_full_name.py is added. utils are fixed

--- bg_shop/account/utils.py
+++ bg_shop/account/utils.py
@@ -9,7 +9,7 @@ def split_full_name(full_name: str) -> list[str]:
     :param full_name: first_name_string + last_name_string
     :return: [first_name, last_name]
     """
-    result = full_name.split(' ')
+    result = full_name.split()
     if len(result) > 2:
         raise ValueError("The full_name contains more than two parts")
     return result
