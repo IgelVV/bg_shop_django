eeef703fa653b514dc6b5abf71e1e2251d712fe9
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-07-13 02:40:04 +0400

    test_dynamic_config_selector.py and test_dynamic_config_service.py are created

--- bg_shop/dynamic_config/selectors.py
+++ bg_shop/dynamic_config/selectors.py
@@ -1,5 +1,4 @@
 from typing import Any
-from django.contrib.auth import get_user_model
 
 from dynamic_config import models
 
--- bg_shop/dynamic_config/services.py
+++ bg_shop/dynamic_config/services.py
@@ -1,7 +1,19 @@
-from typing import Any
-from django.contrib.auth import get_user_model
+from django.conf import settings
 
 from dynamic_config import models
 
 
-User = get_user_model()
+class DynamicConfigService:
+    @staticmethod
+    def set_default_config():
+        d_config = models.DynamicConfig(
+            ordinary_delivery_cost=settings.ORDINARY_DELIVERY_COST,
+            express_delivery_extra_charge=settings.EXPRESS_DELIVERY_EXTRA_CHARGE,
+            boundary_of_free_delivery=settings.BOUNDARY_OF_FREE_DELIVERY,
+            company_info=settings.COMPANY_INFO,
+            legal_address=settings.LEGAL_ADDRESS,
+            main_phone=settings.MAIN_PHONE,
+            main_email=settings.MAIN_EMAIL,
+        )
+        d_config.save()
+
