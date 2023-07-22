fc835f4c5864a51f040620db5742687f1433b948
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-07-22 16:27:10 +0400

    PaymentNumberRegexValidator is fixed

--- bg_shop/payment/validators.py
+++ bg_shop/payment/validators.py
@@ -6,5 +6,5 @@ from django.core.validators import RegexValidator
 class PaymentNumberRegexValidator(RegexValidator):
     """Simple fake card number validator."""
 
-    def __int__(self, message: str):
-        super().__init__(regex=r'^d{8}$', message=message)
+    def __init__(self, message: str):
+        super().__init__(regex=r'^\d{8}$', message=message)
